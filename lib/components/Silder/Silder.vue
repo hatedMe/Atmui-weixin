7792dc46511bf8f94ccd10ea981b733e63e5cdab
Admin <7548764@qq.com>
2018-03-19 23:50:16 +0800

    update compontents

---
+++ lib/components/Silder/Silder.vue
@@ -0,0 +1,27 @@
+
+
+
+<template>
+    <div class="slider-container">
+        <div class="slider-wrapper">
+            <slot></slot>
+        </div>
+        <div class="slide-pagination">
+        </div>
+    </div>
+</template>
+
+
+
+
+<script>
+    export default {
+        name: 'Silder'
+    }
+</script>
+
+
+
+
+
+
