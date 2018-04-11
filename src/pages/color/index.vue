a959735ac4f19e0f506b10172fc0e2a4ce039d98
Atom <7548764@qq.com>
2018-04-11 16:37:40 +0800

    .

---
+++ src/pages/color/index.vue
@@ -0,0 +1,40 @@
+
+
+<template>
+    <div>
+        <p>色彩运用规范，以下就是文字用色 与背景等基本使用做说明</p>
+        <ul class="color-list">
+            <li class="flex">
+                <span></span>
+                <p>
+                    <span>#FF0077</span>
+                    <span>适用于品牌色</span>
+                    <span>用于品牌主色</span>
+                </p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style lang="less">
+    .color-list{
+        li {
+            & > span{
+                width: 50px;
+                height: 50px;
+                background-color: red;
+
+            }
+
+        }
+    }
+</style>
+
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,24 +1,81 @@
 <template>
-<div class="container">
-    {{ motto }}
+    <div>
+        <div class="list">
+    <ul>
+        <li class="list-item">
+            <a class="list-item-inner" href="/pages/color/main">
+                <p class="list-main-title">
+                    <Icon type="icon-remind" size="24" />
+                    <span>Button按钮</span>
+                </p>
+                <p class="list-item-after">
+                    <span>小标题</span>
+                    <Icon type="icon-enter" size="20" />
+                </p>
+            </a>
+        </li>
+        <li class="list-item">
+            <a class="list-item-inner">
+                <p class="list-main-title">
+                    <Icon type="icon-integral" size="24" />
+                    <span>{{ mainTitle }}</span>
+                </p>
+                <p class="list-item-after">
+                    <span>{{ subtitle }}</span>
+                    <Icon type="icon-enter" size="20" />
+                </p>
+            </a>
+        </li>
+    </ul>
+    
 </div>
+
+    <nav class="bartab">
+        <a class="bar-tab-item">
+            <Icon type="icon-remind" size="28" />
+            <span class="tab-label">基础</span>
+        </a>
+        <a class="bar-tab-item">
+            <Icon type="icon-integral" size="28" />
+            <span class="tab-label">组件</span>
+        </a>
+        <a class="bar-tab-item">
+            <Icon type="icon-document" size="28" />
+            <span class="tab-label">示例</span>
+        </a>
+        <a class="bar-tab-item">
+            <Icon type="icon-accessory" size="28" />
+            <span class="tab-label">关于</span>
+        </a>
+    </nav>
+
+    </div>
 </template>
 
 <script>
+
+import Icon from '../../Atmui/components/Icon/Icon.vue';
 
 export default {
     data() {
         return {
             motto: 'Hello World',
-            userInfo: {}
+            userInfo: {},
+            mainTitle : 'text',
+            subtitle : 'miniText'
         }
     },
-    mounted () {
-        console.log( 456 );
+    components: {
+        Icon
+    },
+    mounted() {
+        console.log(456);
     }
 }
 </script>
 
 <style>
-
+p {
+    font-size: 20px;
+}
 </style>
