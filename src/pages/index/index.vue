4acb3af8aa46beb11d5143957cef6249daff2e80
Atom <7548764@qq.com>
2018-04-11 13:48:59 +0800

    weixin mini app atmui

---
+++ src/pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="container">
+    {{ motto }}
+</div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            motto: 'Hello World',
+            userInfo: {}
+        }
+    },
+    mounted () {
+        console.log( 456 );
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/list/index.vue
@@ -0,0 +1,19 @@
+
+
+<template>
+  <div class="counter-warp">
+    <p>Vuex </p>
+    <p>
+      <button>+</button>
+      <button>-</button>
+    </p>
+    <a href="/pages/index/main" class="home">去往首页</a>
+  </div>
+</template>
+
+
+<script>
+export default {
+
+}
+</script>
